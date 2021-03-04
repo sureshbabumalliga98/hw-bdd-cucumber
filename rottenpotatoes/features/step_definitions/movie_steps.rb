a159ea305741b35952a43f5dfb3710ab6978cdad
Malliga98 <sureshbabumalliga98@gmail.com>
2021-03-04 03:09:56 +0000

    Part 1 complete

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create movie
   end
-  fail "Unimplemented"
+  ##fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +19,10 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  e1_index = page.body.index(e1)
+  e2_index = page.body.index(e2)
+  expect (e1_index < e2_index)
+  ##fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +33,20 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  ##fail "Unimplemented"
+   rating_list.split(", ").each do |r|
+    field = "ratings_#{r}"
+    if uncheck
+      uncheck(field)
+    else
+      check(field)
+    end
+  end
 end
+
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  ##fail "Unimplemented"
+  expect(page).to have_xpath(".//table[@id='movies']//tr", :count=>11)
 end
